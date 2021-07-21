56296036b2289bc6f4dfeb9de1ae92c5407ddc41
heidichen90 <toddypet@gmail.com>
2021-07-21 23:38:46 +0100

    list down todo step on get request

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
 const routes = require("./routes");
+require("./config/mongoose");
 
 const PORT = 3000;
 
+//set up handlebars
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 
+//set up body-parser
+app.use(express.urlencoded({ extended: true }));
+
+//set up router
 app.use(routes);
 
 app.listen(PORT, () => {
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const Url = require("../../models/url");
 
 router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.post("/");
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const url = req.body.inputUrl;
+  let shortenResult = "";
+  //look up in db to check if exisitng record
+  //need to move it out as a function and then do a await here
+  Url.findOne({ url })
+    .lean()
+    .then((shortenurl) => {
+      shortenResult = shortenurl;
+      if (!shortenurl) {
+        //generate a shorten url and make sure it's not exist
+        //save it to db
+      }
+      //take the shorten url and render the result
+      console.log(shortenResult);
+    });
+});
 
 module.exports = router;
